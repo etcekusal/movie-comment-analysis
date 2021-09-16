911c6d1ded4788d4ead9d801643bcd073c23885e
etcekusal <84451099+etcekusal@users.noreply.github.com>
2021-09-16 11:40:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ TAG_RE = re.compile(r'<[^>]+>')
  
 def remove_tags(text):
     return TAG_RE.sub('',text)
-model = tf.keras.models.load_model("/content/drive/MyDrive/Text Analysis /")
+model = tf.keras.models.load_model("https://drive.google.com/drive/folders/1UF6o5vqBsmDNV7gv3gnvWwVWYFlr4_EX?usp=sharing")
 import pickle
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
